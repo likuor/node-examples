6444e5d9fdf4836b2c3224244864ecf7b3df0a22
Koki Sakai <likuor0528@gmail.com>
2022-09-08 10:44:01 -0700

    Add server

--- w1/d1/server.js
+++ w1/d1/server.js
@@ -1,11 +1,23 @@
 const http = require('http');
+const users = [
+  { name: 'Vini', country: 'Brazil' },
+  { name: 'Koki', country: 'Japan' },
+  { name: 'Arthur', country: 'Brazil' },
+];
+
+const products = [
+  { name: 'Pen', jenre: 'stationary' },
+  { name: 'Pineapple', jenre: 'fruits' },
+  { name: 'Apple', jenre: 'fruits' },
+  { name: 'Pen', jenre: 'stationary' },
+];
 
 const server = http.createServer((request, response) => {
   console.log('URL', request.url);
 
   if (request.url === '/test') {
-    response.writeHead(200, { 'Content-type': 'text/html' });
-    response.write('<h1>Hello Client in test!!!!!!</h1>');
+    response.writeHead(200, { 'Content-type': 'application/json' });
+    response.write(JSON.stringify({ message: 'Test' }));
   }
 
   if (request.url === '/cats') {
@@ -15,7 +27,6 @@ const server = http.createServer((request, response) => {
         'https://content.wepik.com/statics/3272058/preview-page0.jpg' +
         '>'
     );
-    response.end();
   }
 
   if (request.url === '/dogs') {
@@ -25,21 +36,22 @@ const server = http.createServer((request, response) => {
         'https://www.kindpng.com/picc/m/120-1209732_dog-head-png-dog-meme-face-png-transparent.png' +
         '>'
     );
-    response.end();
   }
 
   if (request.url === '/users') {
-    response.setHeader('Content-type', 'text/html');
-    response.write('<ul><li>Vini</li><li>Koki</li><li>Arthur</li></ul>');
-    response.end();
+    response.writeHead(200, { 'Content-type': 'text/html' });
+    users.map((user) => {
+      response.write(`<ul><li>${user.name} is from ${user.country}</li></ul>`);
+    });
   }
 
   if (request.url === '/products') {
-    response.setHeader('Content-type', 'text/html');
-    response.write(
-      '<ul><li>Pen</li><li>Pineapple</li><li>Apple</li><li>Pen</li></ul>'
-    );
-    response.end();
+    response.writeHead(200, { 'Content-type': 'text/html' });
+    products.map((product) => {
+      response.write(
+        `<ul><li>Jenre of ${product.name} is ${product.jenre}</li></ul>`
+      );
+    });
   }
 
   response.end();
