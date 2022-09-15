c445b7721f6a40936d0026a1d40f3bd3db6116cc
Koki Sakai <likuor0528@gmail.com>
2022-09-15 10:35:52 -0700

    Add exercise

--- w2/d2/exercise.js
+++ w2/d2/exercise.js
@@ -1,61 +1,83 @@
-const cookieParser = require("cookie-parser");
-const express = require("express");
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const users = {
   test: {
-    name: "Test",
-    username: "test",
-    password: "1234",
+    name: 'Test',
+    username: 'test',
+    password: '$2b$12$Z4wPO.1ByHEehpCKCn3ElOsCmFE/Va4z98u6pSiBbRDLQv.u37miW',
   },
 };
 
-app.get("/register", (req, res) => {
-  res.render("register");
+const saltRounds = 12;
+
+const hashPassword = async (password, saltRounds) => {
+  console.time('hashing');
+  const salt = await bcrypt.genSalt(saltRounds);
+  console.log('salt', salt);
+  const hash = await bcrypt.hash(password, salt);
+  console.timeEnd('hashing');
+  console.log('hash', hash);
+  //   const hash = await bcrypt.hash(password, saltRounds);
+  return hash;
+};
+
+app.get('/register', (req, res) => {
+  res.render('register');
 });
 
-app.post("/register", (req, res) => {
-  console.log("body", req.body);
+app.post('/register', (req, res) => {
+  console.log('body', req.body);
+  req.body.password = hashPassword(req.body.password, saltRounds);
   const { name, username, password } = req.body;
-  console.log("username", username);
+  console.log('username', username);
   users[username] = { name, username, password };
 
-  console.log("users", users);
-  res.cookie("username", username);
-  res.send("user created");
+  console.log('users', users);
+  res.cookie('username', username);
+  res.send('user created');
 });
 
-app.get("/login", (req, res) => {
-  res.render("login");
+app.get('/login', (req, res) => {
+  res.render('login');
 });
 
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const receivedUsername = req.body.username;
   const receivedPassword = req.body.password;
   const user = users[receivedUsername];
-  if (!user) return res.send("invalid username");
+  if (!user) return res.send('invalid username');
+
+  console.log(users.test.password);
+
+  bcrypt.compare(users.test.password, users.test.password).then((result) => {
+    console.log('result', result);
+  });
+
   if (user.password === receivedPassword) {
-    res.cookie("username", user.username);
-    return res.send("You are logged in");
+    res.cookie('username', user.username);
+    return res.send('You are logged in');
   }
-  res.send("invalid password");
+  res.send('invalid password');
 });
 
-app.get("/profile", (req, res) => {
+app.get('/profile', (req, res) => {
   const username = req.cookies.username;
-  if (!username) return res.redirect("/login");
+  if (!username) return res.redirect('/login');
   const user = users[username];
-  res.render("profile", { username: user.username, password: user.password });
+  res.render('profile', { username: user.username, password: user.password });
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/login");
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/login');
 });
 
-app.listen(8080, () => console.log("server running 8080"));
+app.listen(8081, () => console.log('server running 8081'));
--- w2/d3/bcrypt.js
+++ w2/d3/bcrypt.js
@@ -1,14 +1,14 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
 
 const user = {
-  username: "admin",
-  password: "password",
+  username: 'admin',
+  password: 'password',
 };
 
 const users = [
   {
-    username: "admin",
-    password: "$2b$12$V7NG0tOiemLbFvuU3t1JOeCgyH8TYP5I2XU5.CdrtWx3YLT3T29pm",
+    username: 'admin',
+    password: '$2b$12$V7NG0tOiemLbFvuU3t1JOeCgyH8TYP5I2XU5.CdrtWx3YLT3T29pm',
   },
 ];
 
@@ -17,12 +17,12 @@ const saltRounds = 12;
 
 // Hashing the password
 const hashPassword = async (password, saltRounds) => {
-  console.time("hashing");
+  console.time('hashing');
   const salt = await bcrypt.genSalt(saltRounds);
-  console.log("salt", salt);
+  console.log('salt', salt);
   const hash = await bcrypt.hash(password, salt);
-  console.timeEnd("hashing");
-  console.log("hash", hash);
+  console.timeEnd('hashing');
+  console.log('hash', hash);
   //   const hash = await bcrypt.hash(password, saltRounds);
   return hash;
 };
@@ -30,6 +30,6 @@ const hashPassword = async (password, saltRounds) => {
 hashPassword(user.password, saltRounds);
 
 // Compare the password and return true or false
-bcrypt.compare("test", users[0].password).then((result) => {
-  console.log("result", result);
+bcrypt.compare('test', users[0].password).then((result) => {
+  console.log('result', result);
 });
